c2eacc865c926c0508493235ac2c862d96a13895
Mrfefis <https6mil@gmail.com>
2024-05-27 19:22:13 -0300

    Ahora no hay que copiar mas header-nav-footer

--- clases.js
+++ clases.js
@@ -1,16 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Seleccionar todos los elementos <a> dentro del div con la clase "clases"
-    const links = document.querySelectorAll('.clases a');
-    const clases = document.querySelector('div.clases');
 
-    // Oculta la seleccion de clases
+// Oculta la seleccion de clases
+function Ocultar(links, clases){
     links.forEach(link => {
         link.addEventListener('click', function(event){
             clases.style.display = 'none';
             const materia = link.classList[0];
             const contenedor = document.querySelector('div.clase.' + materia);
+            const botonSeleccion = document.querySelector('a.seleccionar');
             contenedor.style.display = 'flex';
+            botonSeleccion.style.display = 'block';
         });
     });
-    // Muestra la clase seleccionada
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleccionar todos los elementos <a> dentro del div con la clase "clases"
+    const links = document.querySelectorAll('.clases a');
+    const clases = document.querySelector('div.clases');
+    Ocultar(links, clases);
+    const botonSeleccion = document.querySelector('a.seleccionar');
+    botonSeleccion.addEventListener('click',function (event){
+        botonSeleccion.style.display = 'none';
+        clases.style.display = 'grid';
+         links.forEach(link => {
+             const materia = link.classList[0];
+             const contenedor = document.querySelector('div.clase.' + materia);
+             contenedor.style.display = 'none';
+        });
+    });
 });
---
+++ header-nav.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Carga el header y el nav en las clases de apoyo
+    fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('header').innerHTML = data;
+    });
+    fetch('nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('nav').innerHTML = data;
+    });
+    fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('footer').innerHTML = data;
+    });
+});
